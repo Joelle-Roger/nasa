cb7642f45fb9bc9cee32cdd50f34fc6f295a1262
Joelle-Roger <joelleamal@gmail.com>
2024-09-23 12:17:58 +0300

    BEM

--- nasa.js
+++ nasa.js
@@ -7,7 +7,7 @@ export async function picture() {
         const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}${date?`&date=${date}`: ''}`)
         if(!response.ok){
             console.log('could not fetch')
-            return
+            return 
         }
         const data = await response.json()
         console.log(data)
