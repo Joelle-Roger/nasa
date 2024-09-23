c2be3164878322a782b22466f0b7238a1d5938cf
Joelle-Roger <joelleamal@gmail.com>
2024-09-23 10:25:49 +0300

    BEM

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.querySelector("#app").innerHTML = `
     <h1 class="container__title"> Astronomy Picture of the Day</h1>
     <input type="date" id="date-input" class="container__input">
     <button id="picture-btn" class="container__button">Display Picture</button>
-    <div id="apod-container"class="container__apod"></div>
+    <div id="apod-container" class="container__apod"></div>
   </div>
 `;
 const pictureBtn = document.getElementById('picture-btn')
