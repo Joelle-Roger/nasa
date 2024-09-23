b2338771a1b1164801c2699dbdc92f2f8cf6b4de
Joelle-Roger <joelleamal@gmail.com>
2024-09-23 16:18:15 +0300

    style

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import {picture } from "./nasa.js"
 
 document.querySelector("#app").innerHTML = `
   <div class="container">
-    <h1 class="container__title"> Astronomy Picture of the Day</h1>
+  <div class="container__header">
+      <h1 class="container__title"> Astronomy Picture of the Day</h1>
     <input type="date" id="date-input" class="container__input">
     <button id="picture-btn" class="container__button">Display Picture</button>
+    </div>
     <div id="apod-container" class="container__apod"></div>
   </div>
 `;
