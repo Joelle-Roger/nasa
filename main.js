8fdc82ffaf5a59b0315b973ac3426ebdf5e1426d
Joelle-Roger <joelleamal@gmail.com>
2024-09-23 10:03:44 +0300

    container BEM

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.querySelector("#app").innerHTML = `
   <div class="container">
     <h1 class="container__title"> Astronomy Picture of the Day</h1>
     <input type="date" id="date-input" class="container__input">
-    <button id="picture-btn">Display Picture</button>
+    <button id="picture-btn" class="container__button">Display Picture</button>
     <div id="apod-container"></div>
   </div>
 `;
