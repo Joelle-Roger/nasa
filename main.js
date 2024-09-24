8d6e7808264004288c70efba86fe884178ba3103
Joelle-Roger <joelleamal@gmail.com>
2024-09-24 08:54:18 +0300

    x

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ document.querySelector("#app").innerHTML = `
   <div class="container">
   <div class="container__header">
       <h1 class="container__title"> Astronomy Picture of the Day</h1>
+      <p class="container__text">Choose a date to reveal a picture with a fun explanation!</p>
     <input type="date" id="date-input" class="container__input">
     <button id="picture-btn" class="container__button">Display Picture</button>
     </div>
