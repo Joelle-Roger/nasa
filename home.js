7ab8f8059fff0121288cc5517e794134439bfbb6
Joelle-Roger <joelleamal@gmail.com>
2024-09-24 09:15:17 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,3 @@
+document.querySelector('#home').innerHTML=`<h1>Welcome to the NASA API App!</h1>
+        <p>Explore the Astronomy Picture of the Day and learn about amazing space content.</p>
+        <a href="index.html" class="button">Start Exploring</a>`
