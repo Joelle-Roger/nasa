2aec15b157310db9fa5d4e312cbf1f1b6270f8df
Joelle-Roger <joelleamal@gmail.com>
2024-09-25 11:58:30 +0300

    animations with pic and text

--- nasa.js
+++ nasa.js
@@ -7,10 +7,8 @@ export async function picture() {
     const apodContainer = document.querySelector('.container__apod')
     apodContainer.innerHTML='';
     message.textContent=''
-
     const inputDate = new Date(date);
     message.className='container__apod-error'
-    
     if (inputDate < firstDate) {
         message.textContent = `The selected date is before the first available date (June 16, 1995).`
         message.style.color = 'red';
@@ -20,7 +18,6 @@ export async function picture() {
     try{
         const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}${date?`&date=${date}`: ''}`
         const response = await fetch(url)
-        
             if(!response.ok){
                console.log('could not fetch')
                 return;
@@ -35,40 +32,23 @@ export async function picture() {
 
                 const apodContent = document.createElement('div');
                 apodContent.className = 'container__apod-content'
-    
+
                 const apodImage = document.createElement('img');
                 apodImage.src = data.url;
                 apodImage.alt = "Astronomy Picture of the Day";
-                apodImage.style.width='100%';
                 apodImage.className='container__apod-picture'
-                
-    
+
+                const start=0;
+                const end=500;
                 const apodExp = document.createElement('p')
-                apodExp.className='container__apod-text'
-                const fullText = data.explanation
-                const maxLength = 100;
-                const truncate = fullText.length > maxLength ? `${fullText.substring(0,maxLength)}<br><span class="container__apod-seeMore">See More</span>` : fullText
-                apodExp.innerHTML=`<strong>Explanation:</strong> ${truncate}`
-                
+                apodExp.className = 'container__apod-text'
+                const fullText = data.explanation.substring(start,end);
+                apodExp.innerHTML = `<strong>Explanation:</strong> ${fullText}`
                 apodContent.appendChild(apodImage)
                 apodContent.appendChild(apodExp)
-
                 apodContainer.appendChild(apodContent)
-
-    
-                if(fullText.length > maxLength){
-                    const seeMore = document.querySelector('.container__apod-seeMore')
-                    seeMore.addEventListener('click',(e)=>{
-                        e.preventDefault()
-                        apodExp.innerHTML=`<strong>Explanation:</strong> ${fullText}`
-                    })
-                }
-                
             }
-    
-        
         }catch(error){
             console.error(error)
         }
-        
 }
